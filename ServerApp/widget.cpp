24fc29751bd197183ddadaed3169cdc55fff6ff0
Taewon Kim <55828162+kimtae9217@users.noreply.github.com>
2024-10-10 20:19:02 +0900

    TCP 서버 만들기(QtNetwork 사용)

--- ServerApp/widget.cpp
+++ ServerApp/widget.cpp
@@ -1,14 +1,66 @@
 #include "widget.h"
-#include "ui_widget.h"
+#include <QtWidgets>
+#include <QtNetwork>
 
 Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-    , ui(new Ui::Widget)
+    : QWidget(parent), tcpServer(nullptr)
 {
-    ui->setupUi(this);
+    statusLabel = new QLabel("'Start Server' 버튼을 눌러주세요!", this);
+    startButton = new QPushButton("Start Server", this);
+    stopButton = new QPushButton("Stop Server", this);
+
+    connect(startButton, &QPushButton::clicked, this, &Widget::startServer);
+    connect(stopButton, &QPushButton::clicked, this, &Widget::stopServer);
+
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->addWidget(statusLabel);
+    layout->addWidget(startButton);
+    layout->addWidget(stopButton);
+
+    setLayout(layout);
+    setWindowTitle("Simple TCP Server");
+
+    updateButtonStates();
 }
 
-Widget::~Widget()
+void Widget::startServer()
 {
-    delete ui;
+    tcpServer = new QTcpServer(this);
+    if (!tcpServer->listen(QHostAddress::Any, 8080)) {
+        QMessageBox::critical(this, tr("TCP Server"),
+                              tr("Unable to start the server: %1.")
+                                  .arg(tcpServer->errorString()));
+        delete tcpServer;
+        tcpServer = nullptr;
+        return;
+    }
+
+    connect(tcpServer, &QTcpServer::newConnection, this, &Widget::clientConnect);
+    statusLabel->setText("Server is running on port 8080");
+    updateButtonStates();
+}
+
+void Widget::stopServer()
+{
+    if (tcpServer) {
+        tcpServer->close();
+        delete tcpServer;
+        tcpServer = nullptr;
+    }
+    statusLabel->setText("Server is stopped");
+    updateButtonStates();
+}
+
+void Widget::clientConnect()
+{
+    QTcpSocket *clientConnection = tcpServer->nextPendingConnection();
+    connect(clientConnection, &QAbstractSocket::disconnected,
+            clientConnection, &QObject::deleteLater);
+    statusLabel->setText("New client connected");
+}
+
+void Widget::updateButtonStates()
+{
+    startButton->setEnabled(tcpServer == nullptr);
+    stopButton->setEnabled(tcpServer != nullptr);
 }
--- ServerApp/widget.h
+++ ServerApp/widget.h
@@ -2,22 +2,28 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QTcpServer>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class Widget;
-}
-QT_END_NAMESPACE
+class QLabel;
+class QPushButton;
 
 class Widget : public QWidget
 {
     Q_OBJECT
-
 public:
     Widget(QWidget *parent = nullptr);
-    ~Widget();
+
+private slots:
+    void startServer();
+    void stopServer();
+    void clientConnect();
 
 private:
-    Ui::Widget *ui;
+    QLabel *statusLabel;
+    QPushButton *startButton;
+    QPushButton *stopButton;
+    QTcpServer *tcpServer;
+    void updateButtonStates();
 };
+
 #endif // WIDGET_H
